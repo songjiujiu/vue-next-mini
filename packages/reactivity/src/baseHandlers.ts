455a4e57cb0322aaabc0b1285d80673d1ef8722e
songxs0 <2032824853@qq.com>
2026-04-23 17:28:12 +0800

    feat:收集依赖和触发依赖功能

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,14 +1,18 @@
+import { track, trigger } from "./effect"
 const get = createGetter()
 function createGetter() {
-    return function get (target: object, key: string | symbol, receiver: object) {
-        const res = Reflect.get(target,key,receiver)
+    return function get(target: object, key: string | symbol, receiver: object) {
+        const res = Reflect.get(target, key, receiver)
+        track(target, key)//收集依赖
         return res
     }
 }
 const set = createSetter()
 function createSetter() {
-    return function set ( target: Record<string | symbol, unknown>,key: string | symbol,value: unknown,receiver: object) {
-
+    return function set(target: object, key: string | symbol, value: unknown, receiver: object) {
+        const res = Reflect.get(target, key, value, receiver)
+        trigger(target, key, value )//触发依赖
+        return res
     }
 }
 export const mutableHandlers: ProxyHandler<object> = {
